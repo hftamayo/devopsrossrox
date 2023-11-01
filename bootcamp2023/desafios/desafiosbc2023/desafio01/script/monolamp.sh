e0e7769cf0824cfbbdcfa767afb2951368bd4d21
hftamayo <hftamayo@gmail.com>
2023-11-01 13:13:05 -0600

    script01: passing and validate data via stdin

---
+++ bootcamp2023/desafios/desafiosbc2023/desafio01/script/monolamp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#stage 0: instructions
+echo "==================================================================================="
+echo "Monolith LAMP application deployment automation script"
+echo "This version is for Debian based distro"
+echo "please run the script like this: monolamp <db_admin_password> <user_app_password>"
+echo "do not forget you need to have admin creds to run this"
+echo "==================================================================================="
+
+#validate if two params were passed as expected
+if [ $# -ne 2]; then
+	echo "please type both db_admin and user_app passwords"
+	exit 1
+fi
+
+db_admin=$1
+user_app=$2
+
+echo "your creds are: dbadmin: $db_admin and user_app: $user_app. Press 1 to continue, any other key to abort"
+read continue
+
+if [ "$continue" != "1" ]; then
+	echo "process aborted correctly"
+	exit 1
+fi
+
+echo "stage 1: please wait while installing the needed packages"
+
