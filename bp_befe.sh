959941bff3b066a9efb483811e1c43529442476b
hftamayo <hftamayo@gmail.com>
2023-11-24 13:44:27 -0600

    polishing build and push script part1

--- bp_befe.sh
+++ bp_befe.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+#stage 0: enviro vars
+BC_PROJECT="295devops"
+HOME_PROJECT="desafio02"
+GH_REPO="https://github.com/hftamayo/devopsrossrox.git"
+BRANCH_REPO="desafio02"
+
+
 #stage 1: cloning the repo
+cd $HOME
 
+if [ -d $BC_PROJECT ]; then
+	cd $BC_PROJECT
+else
+	mkdir $BC_PROJECT
+	cd $BC_PROJECT
+fi
 
-#Build and Push BackEnd 295fullstack
+if [ -d $HOME_PROJECT ]; then
+	cd $HOME_PROJECT
+	git checkout $BRANCH_REPO
+	git pull
+else
+	git clone $GH_REPO $BRANCH_REPO
+	cd $BRANCH_REPO
+	git checkout $BRANCH_REPO
+fi
+
+#stage 2: Build and Push BackEnd 295fullstack
 docker build -t 295fs_be:stable Dockerfile.be
 sleep 5
 
@@ -17,3 +41,6 @@ docker push 295fs_be:stable
 sleep 5
 
 docker push 295fs_fe:stable
+sleep 5
+
+echo "BUILD AND PUSH script finished"
