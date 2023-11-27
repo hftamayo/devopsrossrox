5ded4c1ab30cc435c7f24555cb58759327b1f056
hftamayo <hftamayo@gmail.com>
2023-11-27 11:35:17 -0600

    295words: API Dockerfile

---
+++ 295words-docker/Dockerfile.java
@@ -0,0 +1,17 @@
+#################### MAVEN BUILD STAGE ####################
+
+FROM maven:3.8.5-openjdk-17 AS build
+RUN mkdir -p /opt/295words; exit 0
+
+WORKDIR /opt/295words
+COPY api/src ./src
+COPY api/pom.xml .
+RUN mvn clean package
+
+################# JDK RUN STAGE ################### 
+FROM openjdk:17
+WORKDIR /opt/295words
+COPY --from=build /opt/295words/target/words.jar ./words001.jar
+ENTRYPOINT ["java", "-jar", "./words001.jar"]
+
+
