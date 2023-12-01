3060f7cfc17de70ee93a019abcac7f5c7b25b364
hftamayo <hftamayo@gmail.com>
2023-12-01 14:59:27 -0600

    cleaning the Dockerfile using the fat jar build

--- 295words-docker/Dockerfile.java
+++ 295words-docker/Dockerfile.java
@@ -6,14 +6,12 @@ RUN mkdir -p /opt/295words; exit 0
 
 WORKDIR /opt/295words
 COPY api/src ./src
-COPY api/target ./target
 COPY api/pom.xml .
 RUN mvn clean package
 
 ################# JDK RUN STAGE ################### 
 FROM amazoncorretto:17
 WORKDIR /opt/295words
-COPY --from=build /opt/295words/target/words.jar ./words001.jar
+COPY --from=build /opt/295words/target/words-jar-with-dependencies.jar ./words001.jar
+
 ENTRYPOINT ["java", "-jar", "./words001.jar"]
-
-
