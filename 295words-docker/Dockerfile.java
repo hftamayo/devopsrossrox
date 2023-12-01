5b4757390b8aa0aaf87d1117284dfd2299fad738
hftamayo <hftamayo@gmail.com>
2023-12-01 10:30:47 -0600

    295words: copying target directory

--- 295words-docker/Dockerfile.java
+++ 295words-docker/Dockerfile.java
@@ -1,10 +1,12 @@
 #################### MAVEN BUILD STAGE ####################
 
 FROM maven:3.8.5-openjdk-17 AS build
+
 RUN mkdir -p /opt/295words; exit 0
 
 WORKDIR /opt/295words
 COPY api/src ./src
+COPY api/target ./target
 COPY api/pom.xml .
 RUN mvn clean package
 
