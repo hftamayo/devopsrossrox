f9a9db4b3acf154eac15b68e88897def38089fa8
hftamayo <hftamayo@gmail.com>
2023-11-30 22:21:44 -0600

    295words: using amazoncorretto for building

--- 295words-docker/Dockerfile.java
+++ 295words-docker/Dockerfile.java
@@ -9,7 +9,7 @@ COPY api/pom.xml .
 RUN mvn clean package
 
 ################# JDK RUN STAGE ################### 
-FROM openjdk:17
+FROM amazoncorretto:17
 WORKDIR /opt/295words
 COPY --from=build /opt/295words/target/words.jar ./words001.jar
 ENTRYPOINT ["java", "-jar", "./words001.jar"]
