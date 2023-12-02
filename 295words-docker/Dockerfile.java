7e41d6b0c5c03260ab90e4b6063033e55b1b926e
hftamayo <hftamayo@gmail.com>
2023-12-01 19:50:56 -0600

    exposing port 8080

--- 295words-docker/Dockerfile.java
+++ 295words-docker/Dockerfile.java
@@ -1,6 +1,6 @@
 #################### MAVEN BUILD STAGE ####################
 
-FROM maven:3.8.5-openjdk-17 AS build
+FROM maven:3.8.5-openjdk-18 AS build
 
 RUN mkdir -p /opt/295words; exit 0
 
@@ -10,12 +10,12 @@ COPY api/pom.xml .
 RUN mvn clean package
 
 ################# JDK RUN STAGE ################### 
-FROM amazoncorretto:17
+FROM amazoncorretto:18
 RUN yum  install -y net-tools
 
 WORKDIR /opt/295words
 COPY --from=build /opt/295words/target/words-jar-with-dependencies.jar ./words001.jar
 
-EXPOSE 8002
+EXPOSE 8080
 
 ENTRYPOINT ["java", "-jar", "./words001.jar"]
