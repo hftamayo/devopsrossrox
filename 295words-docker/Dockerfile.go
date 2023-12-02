aba8da8c7a3163ee4c9766648306aaaa8478aa29
hftamayo <hftamayo@gmail.com>
2023-12-01 20:26:49 -0600

    second version of Dockerfile.go

--- 295words-docker/Dockerfile.go
+++ 295words-docker/Dockerfile.go
@@ -2,17 +2,19 @@ FROM golang:1.18-alpine AS builder
 
 WORKDIR /app
 
-COPY web/. .
+COPY web/dispatcher.go .
+COPY web/go.mod .
+COPY web/static ./static
 
-RUN go build -o main .
+RUN go build -o dispatcher .
 
 FROM alpine:latest
 
 WORKDIR /app
 
-COPY --from=builder /app/main ./
-COPY --from=builder /app/public ./web/static
+COPY --from=builder /app/dispatcher ./
+COPY --from=builder /app/static ./static
 
 EXPOSE 8088
 
-CMD ["./main"]
+CMD ["./dispatcher"]
