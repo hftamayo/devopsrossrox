d74732948c3747bceac5976784fd86fc3f229c3a
hftamayo <hftamayo@gmail.com>
2023-12-06 21:36:45 -0600

    version of data import compatible with mongo 4.x

--- 295topics-fullstack/db/mongo-initutf8.js
+++ 295topics-fullstack/db/mongo-initutf8.js
@@ -1,26 +1,21 @@
 // mongo-script.js
 
-const dbName = 'TopicstoreDb';
+const dbName = 'TopicstoreDB';
 const collectionName = 'Topics';
-const db = db.getSiblingDB(dbName);
+
+const conn = new Mongo();
+const db = conn.getDB(dbName);
 
 db.createCollection(collectionName);
-function convertToUTF8(str) {
-   return Buffer.from(str, 'latin1').toString('utf8');
-}
-
-const data = [
-    { "Name": "chivo01" },
-    { "Name": "chivo02" },
-    { "Name": "chivo03" },
-    { "Name": "chivo04" },
-    { "Name": "chivo05" },
-    { "Name": "chivo06" },
-    { "Name": "chivo07" },
-    { "Name": "chivo08" },
-    { "Name": "chivo09" }
-];
-
-const convertedData = data.map(item => ({ "Name": convertToUTF8(item.Name) }));
-
-db[collectionName].insertMany(convertedData);
+db[collectionName].insertMany([
+    { "Name": "Desplegando el Futuro: Innovación Continua con Docker y DevOps" },
+    { "Name": "Navegando en la Ola de la Innovación: Estrategias Docker y Prácticas DevOps" },
+    { "Name": "Construyendo Puentes hacia el Mañana: Innovación en la Nube con Docker y DevOps" },
+    { "Name": "DevOps en Acción: Transformando Ideas en Innovación con Docker" },
+    { "Name": "Dockerizando el Camino hacia la Eficiencia: Innovación Impulsada por DevOps" },
+    { "Name": "Reimaginando el Desarrollo: Historias de Éxito en Innovación con Docker y DevOps" },
+    { "Name": "Más Allá de los Límites: Explorando la Innovación a través de Docker y Prácticas DevOps" },
+    { "Name": "Innovación en el Ciclo de Vida del Software: Una Odisea con Docker y DevOps" },
+    { "Name": "Empoderando la Innovación: Estrategias Contemporáneas con Docker y DevOps" }    
+    
+]);
