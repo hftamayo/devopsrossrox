5f043c8f72bea7bfb130b35d096c3bb33dec4c7c
hftamayo <hftamayo@gmail.com>
2023-12-08 10:37:53 -0600

    adjusting data volume and deploy script

--- bootcamp2023/desafios/desafio02/295words-docker/295wordsdeploy.sh
+++ bootcamp2023/desafios/desafio02/295words-docker/295wordsdeploy.sh
@@ -1,5 +1,9 @@
 cd ~/295devops/desafio02/bootcamp2023/desafios/desafio02/295words-docker
 
+mkdir data
+mkdir data/pgdata
+
+sleep 3
 
 docker-compose -p 295words -f docker-compose-images.yml  up -d
 sleep 5
