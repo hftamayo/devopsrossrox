0785fe44ba5c490d1af451ce098b9e49e29f79c3
hftamayo <hftamayo@gmail.com>
2023-12-07 20:30:49 -0600

    adjust in volume db service and script for deployment

--- bootcamp2023/desafios/desafio02/295topics-fullstack/295fsdeploy.sh
+++ bootcamp2023/desafios/desafio02/295topics-fullstack/295fsdeploy.sh
@@ -1,5 +1,12 @@
-echo "DEPLOYING CONTAINERS"
-sleep 2
+#!/bin/bash
+
+cd ~/295devops/desafio02/bootcamp2023/desafios/desafio02/295topics-fullstack
+
+mkdir data
+mkdir data/mongodb_data
+
+sleep 3
+
 docker-compose -p 295fs -f docker-compose-onpremise.yml up -d
 
-#docker exec -it mongodb mongo localhost:27017/TopicstoreDB /docker-entrypoint-initdb.d/mongo-initutf8.js
+sleep 2
